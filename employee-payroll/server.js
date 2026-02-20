eb328bc7e5b742e5f052923fff0015703b510494
pragati-2024 <bpragati21@gmail.com>
2026-02-20 09:08:55 +0530

    MINI-PROJECT BY BRIDGE-LAB

--- employee-payroll/server.js
+++ employee-payroll/server.js
@@ -1,11 +1,29 @@
-import express from 'express';
-import userRoute from './router/userRoute.js';
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
+import express from "express";
+import methodOverride from "method-override";
+import path from "path";
+import { fileURLToPath } from "url";
+import pageRoute from "./routes/pageRoute.js";
+import employeeRoute from "./routes/employeeRoute.js";
+import { loggingMiddleware } from "./middleware/loging.js";
 dotenv.config();
-let app=express();
-app.use(express.json());//convert raw json string into js object
-app.use("/api",userRoute);
+const app = express();
 
-app.listen(3000,()=>{
-    console.log(`Server is running on port 3000`);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(methodOverride("_method"));
+
+const port = process.env.PORT || 3000;
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(loggingMiddleware);
+app.use("/", pageRoute);
+app.use("/api", employeeRoute);
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
