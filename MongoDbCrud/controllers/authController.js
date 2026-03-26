e4580a182529693c14d13bfe1fe8e1dbce618a30
pragati-2024 <bpragati21@gmail.com>
2026-03-26 09:19:33 +0530

    databses learning  and forming authcontroller and bcrypting,hashing concept

--- MongoDbCrud/controllers/authController.js
+++ MongoDbCrud/controllers/authController.js
@@ -1,10 +1,14 @@
 import user from "../model/UserSchema.js"
+import bcrypt from "bcrypt"
 export const signup = async(req,res)=>{
     try{
         const{name,email,password} = req.body
+        const salt = await bcrypt.genSalt(10);
+        const hashePassword = await bcrypt.hash(password,salt);
+
         const newUser = await user.create({
-            name,email,password
-        })
+            name,email,password:hashePassword,
+        });
         res.status(200).json({
             message:"user is created",
             newUser,
