84b32e5278bced72e8ab9e41cd5a9ec6a9eb0c4d
pragati-2024 <bpragati21@gmail.com>
2026-03-30 09:37:11 +0530

    learning jwt authentication

--- MongoDbCrud/demo.js
+++ MongoDbCrud/demo.js
@@ -1,28 +1,75 @@
 import express from 'express'
 import cookieParser from 'cookie-parser'
+import jwt from 'jsonwebtoken'
+
 const app = express();
 app.use(cookieParser('my-super-secret-key'));
+
 // phle set krna pdega then get
-app.get('/set-cookie',(req,res)=>{
-    res.cookie('name','rohan',{httpOnly:true});
-    res.send('Cookies has been send!')
-    
-});
-app.get('/get-cookie',(req,res)=>{
-    // agr cookies nai hae toh invalid user
-    if(!req.cookies.name){
+// app.get('/set-cookie',(req,res)=>{
+//     res.cookie('name','rohan',{httpOnly:true});
+//     res.send('Cookies has been send!')
+// });
+
+// simple authentication
+const authMiddleware  = (req,res,next)=>{
+     if(!req.cookies.token){
         return res.send("invalid user");
     }
+    next()
+    // if(!req.cookies.name){
+    //     res.send("invalid user");
+    //     return;
+    // }
+    // next()
+}
+
+app.get('/get-cookie',authMiddleware,(req,res)=>{
+    // agr cookies nai hae toh invalid user
+    // if(!req.cookies.name){
+    //     return res.send("invalid user");
+    // }
     const name = req.cookies.name;
     res.send(`cookie value : ${name}`);
 })
-app.get('/profile',(req,res)=>{
+
+app.get('/set-cookie',(req,res)=>{
     // agr cookies nai hae toh invalid user
-    if(!req.cookies.name){
-        return res.send("invalid user");
+    // if(!req.cookies.name){
+    //     return res.send("invalid user");
+    // }
+    let user = {
+        name:'rohan',
+        email:'rohanb@gmail.com'
     }
-    const name = req.cookies.name;
-    res.send(`Welcome to your profile, ${name}`);
+
+    const token = jwt.sign(user,"qwerty",{expiresIn:"1h"});
+    console.log(token)
+
+    res.cookie("token",token,{httpOnly:true});
+    res.send("Cookies has been settttttt!!!!!!!!!!!11")
+
+    // const name = req.cookies.name;
+    // res.send(`cookie value, ${name}`);
+})
+
+app.get('/profile',authMiddleware,(req,res)=>{
+    // agr cookies nai hae toh invalid user
+    // if(!req.cookies.name){
+    //     return res.send("invalid user");
+    // }
+
+    const token = req.cookies.token;   // 
+    const decode = jwt.verify(token,"qwerty");  // 
+
+    console.log(decode)
+
+    res.send(`Welcome to your profile, ${decode.name}`);
+})
+
+app.get('/logout',authMiddleware,(req,res)=>{
+    res.clearCookie("token")   
+    res.send(`logout is successfull`)
 })
 
 app.listen(3000,()=>console.log("server is running on port 3000"))
